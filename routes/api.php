cc4e162f45306d8daba1e11ae99484d0932ac9e5
Amr Mohamed <amrmuhmedd@gmail.com>
2023-09-17 23:52:56 +0300

    create authentication

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('details', [UserController::class, 'details']);
+});
